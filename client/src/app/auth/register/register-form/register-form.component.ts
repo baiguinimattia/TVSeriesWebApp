61e0c6f8537ce53aa381b5988b65d1da3e5d5d51
Baiguini Mattia <mattia.baiguini@gmail.com>
2020-02-04 02:05:13 +0200

    adding email validation on client

--- client/src/app/auth/register/register-form/register-form.component.ts
+++ client/src/app/auth/register/register-form/register-form.component.ts
@@ -29,6 +29,7 @@ export class RegisterFormComponent implements OnInit {
     this.registerForm = new FormGroup({
       email: new FormControl('', [
         Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
       ]),
       password: new FormControl('', [
         Validators.required,
